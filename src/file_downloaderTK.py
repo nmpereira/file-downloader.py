d4aa2c057bb9f427f226349274497d248f110c99
npereira <Nikhil_Pereira@intuit.com>
2021-08-27 18:05:04 -0400

    Updated functionality for tkinter progressbar

--- src/file_downloaderTK.py
+++ src/file_downloaderTK.py
@@ -1,13 +1,78 @@
 from tkinter import *
 from tkinter import ttk
+from tqdm import tqdm
+import requests
+import sys
 
 root = Tk()
 
 root.title('File Downloader')
-root.geometry("600x160")
+root.geometry("600x200")
+
+chunk_size = 1048576
+
+url = "http://speedtest.tele2.net/100MB.zip"
+
+r = requests.get(url, stream = True)
+
+total_size = int(r.headers['content-length'])
+filename = url.split('/')[-1]
+#bar_format=None
+
+bar_format='{l_bar}{bar}''|{n_fmt}MB/{total:.2f}MB | [Time:{elapsed}<{remaining} {rate_fmt}{postfix}]'
+iterable = r.iter_content(chunk_size = chunk_size) 
+total = total_size/chunk_size
+
+unit = 'MB' 
+bar_format=bar_format
+
+print(url)
+print("Downloading..")
+# with open(filename, 'wb') as f:
+# 	for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'MB', bar_format=bar_format):
+# 		f.write(data)
+
+
+print("Download complete!")
+
+total_length = r.headers.get('content-length')
 
 def step():
-	my_progress.start(10)
+
+	with open(filename, 'wb') as f:
+		dl = 0
+		total_length = r.headers.get('content-length')
+		total_length = int(total_length)
+		for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'MB', bar_format=bar_format):
+			#my_progress.start()
+			dl += len(data)
+			done = int(100 * dl / total_length)
+			my_progress['value']=done
+			root.update_idletasks()
+			f.write(data)
+
+			#my_label.config(text=data)
+
+
+	# with open(filename, "wb") as f:
+	# 	print("Downloading %s" % filename)
+	# 	response = requests.get(url, stream=True)
+	# 	total_length = response.headers.get('content-length')
+
+	# 	if total_length is None: # no content length header
+	# 		f.write(response.content)
+	# 	else:
+	# 		dl = 0
+	# 		total_length = int(total_length)
+	# 		for data in response.iter_content(chunk_size=1024):
+	# 			dl += len(data)
+	# 			f.write(data)
+	# 			done = int(100 * dl / total_length)
+	# 			#sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (100-done)) ) 
+	# 			my_progress['value']=done   
+	# 			root.update_idletasks()
+				#sys.stdout.flush()
+	#my_progress.stop()
 
 def stop():
 	my_progress.stop()
@@ -24,7 +89,8 @@ start_button.pack(pady=10)
 stop_button =Button(root, text='stop', command=stop)
 stop_button.pack(pady=10)
 
-
+my_label =Label(root, text='')
+my_label.pack(pady=10)
 
 
 root.mainloop()
