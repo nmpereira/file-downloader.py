a55a3dc3f5235c80c3c8e9be0dfd2cdd8ebff8d6
npereira <Nikhil_Pereira@intuit.com>
2021-08-27 16:21:55 -0400

    improve progressbar and compile exe

--- src/file_downloader.py
+++ src/file_downloader.py
@@ -10,11 +10,12 @@ r = requests.get(url, stream = True)
 
 total_size = int(r.headers['content-length'])
 filename = url.split('/')[-1]
-
+#bar_format=None
+bar_format='{l_bar}{bar}''|{n_fmt}MB/{total:.2f}MB | [Time:{elapsed}<{remaining} {rate_fmt}{postfix}]'
 print(url)
 print("Downloading..")
 with open(filename, 'wb') as f:
-	for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'MB'):
+	for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'MB', bar_format=bar_format):
 		f.write(data)
 
 
