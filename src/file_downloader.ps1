08b7bdb5095d0a4bcb4c9c8c9867f385eed9b5db
nmpereira <nikhilpereira25@gmail.com>
2021-08-30 22:33:04 -0400

    implemented time labels

--- src/file_downloader.ps1
+++ src/file_downloader.ps1
@@ -1,3 +1,3 @@
-Python file_downloader.py "http://speedtest.tele2.net/1GB.zip" "C:\\gitpersonal\\file_downloader.py"
+Python file_downloader.py "http://speedtest.tele2.net/1GB.zip" "C:\\gitpersonal\\file-downloader.py"
 
 pause
--- src/file_downloaderTK.py
+++ src/file_downloaderTK.py
@@ -15,7 +15,7 @@ root.geometry("600x300")
 chunk_size = 1048576
 
 url = "http://speedtest.tele2.net/1GB.zip"
-directory = "C:\\gitpersonal\\file_downloader.py\\src"
+directory = "C:\\gitpersonal\\file-downloader.py\\src"
 
 r = requests.get(url, stream = True)
 
@@ -28,9 +28,30 @@ bar_format='{l_bar}{bar}''|{n_fmt}MB/{total:.2f}MB | [Time:{elapsed}<{remaining}
 # total = total_size/chunk_size
 global done
 global current_time
-current_byte=0
-done=0
-current_time=0
+global current_byte
+global download_speed
+global total_time
+global time_left
+complete = False
+
+def reset_params():
+	#global complete
+	global done
+	global current_time
+	global current_byte
+	global download_speed
+	global total_time
+	global time_left
+	done=0
+	current_time=0
+	current_byte=0
+	download_speed=0
+	total_time=0
+	time_left=0
+	
+
+	update_label()
+
 # unit = 'MB' 
 # bar_format=bar_format
 
@@ -43,11 +64,12 @@ print("Downloading..")
 
 print("Download complete!")
 
-total_length = r.headers.get('content-length')
+#total_size = int(r.headers.get('content-length'))
 label_percent_complete = Label(root, text='Complete: '+'0'+'%')
-label_total_byte = Label(root, text='total_byte: '+'0'+'MB')
+label_total_size = Label(root, text='total_size: '+'0'+'MB')
 label_current_byte = Label(root, text='current_byte: '+'0'+'MB')
-label_total_time = Label(root, text='total_time: '+'00:00')
+label_time_left = Label(root, text='time_left: '+'00:00')
+label_total_time = Label(root, text='time_left: '+'00:00')
 label_current_time = Label(root, text='current_time: '+'00:00')
 label_download_speed = Label(root, text='download_speed: '+'0'+'MB/s')
 
@@ -55,38 +77,44 @@ def update_label():
 	global label_percent_complete
 	global label_current_time
 	label_percent_complete.config(text='Complete: '+ str(done) + '%')
-	label_total_byte.config(text='total_byte: '+'0'+'MB')
+	label_total_size.config(text='total_size: '+ str(total_size/chunk_size)+'MB')
 	label_current_byte.config(text='current_byte: '+ str(current_byte) +'MB')
-	label_total_time.config(text='total_time: '+'00:00')
-	label_current_time.config(text=f'current_time: '+'00:'+str(current_time))
-	label_download_speed.config(text='download_speed: '+'0'+'MB/s')
+	label_time_left.config(text='time_left: '+str(int(time_left)))
+	label_total_time.config(text='total_time: '+str(int(total_time)))
+	label_current_time.config(text=f'current_time: '+str(int(current_time)))
+	label_download_speed.config(text='download_speed: '+str(download_speed)+'MB/s')
 	#print("done", done)
 
 	#pass
 
 def step():
 	global stop
+	
 	stop=False
 	chunk_size = 1048576
 	r = requests.get(url, stream = True)
 
-	total_size = int(r.headers['content-length'])
+	
 	filename = directory + '\\' + url.split('/')[-1]
 	with open(filename, 'wb') as f:
+		
 		start_time = time.time()
 		dl = 0
 		
-		total_length = r.headers.get('content-length')
-		total_length = int(total_length)
+		total_size = int(r.headers.get('content-length'))
+		
 		for data in tqdm(iterable = r.iter_content(chunk_size = chunk_size), total = total_size/chunk_size, unit = 'MB', bar_format=bar_format):
 			global done
 			global current_time 
 			global current_byte
+			global download_speed
+			global total_time
+			global time_left
 
 			root.update_idletasks()
 			#my_progress.start()
 			dl += len(data)
-			done= int(100 * dl / total_length)
+			done= int(100 * dl / total_size)
 			my_progress['value']=done
 			#label_percent_complete.text=done
 			#print("done", done)
@@ -95,12 +123,16 @@ def step():
 			#print(int(time.time() - start_time))
 			current_time=int(time.time()- start_time)
 			current_byte=dl/chunk_size
+			download_speed=round(((dl//(time.time() - start_time) / 100000)*0.125),2)
+			total_time=round(total_size/((download_speed*100000)/0.125),2)
+			time_left=round(total_time-current_time,2)
 			#tqdm.update()
-			#print(data.format_dict['elapsed'])
 			root.after(1,update_label())
-
 			if stop == True:
 				break
+		
+		
+		
 
 
 			
@@ -109,17 +141,17 @@ def step():
 	# with open(filename, "wb") as f:
 	# 	print("Downloading %s" % filename)
 	# 	response = requests.get(url, stream=True)
-	# 	total_length = response.headers.get('content-length')
+	# 	total_size = response.headers.get('content-length')
 
-	# 	if total_length is None: # no content length header
+	# 	if total_size is None: # no content length header
 	# 		f.write(response.content)
 	# 	else:
 	# 		dl = 0
-	# 		total_length = int(total_length)
+	# 		total_size = int(total_size)
 	# 		for data in response.iter_content(chunk_size=1024):
 	# 			dl += len(data)
 	# 			f.write(data)
-	# 			done = int(100 * dl / total_length)
+	# 			done = int(100 * dl / total_size)
 	# 			#sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (100-done)) ) 
 	# 			my_progress['value']=done   
 	# 			root.update_idletasks()
@@ -135,8 +167,9 @@ my_progress.pack(pady=20)
 
 label_percent_complete.pack()
 label_current_byte.pack()
-label_total_byte.pack()
+label_total_size.pack()
 label_current_time.pack()
+label_time_left.pack()
 label_total_time.pack()
 label_download_speed.pack()
 
