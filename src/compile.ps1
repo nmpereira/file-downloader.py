5b755214bb8f3159440e79c47e85425709d04dea
nmpereira <nikhilpereira25@gmail.com>
2021-09-01 13:08:22 -0400

    Create venv for compile process

--- src/compile.ps1
+++ src/compile.ps1
@@ -1,3 +1,11 @@
+.\build_env\Scripts\activate.ps1
+
+start-sleep 5
+
+pip install -r requirements.txt
+
+start-sleep 5
+
 pyinstaller --noconfirm --onefile --windowed "C:/gitpersonal/file-downloader.py/src/file_downloaderTK.py"
 
 pause
